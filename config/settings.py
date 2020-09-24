b97abd9a50b04c072b0492f9d59d011fcd625c54
judascrow <judascrow1@gmail.com>
2020-09-24 22:44:47 +0700

    [Add] Sharing posts by email

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
